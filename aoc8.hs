0528c8180b3e6fcba2a797c63b75e37f31d134c4
Manuel Eberl <manuel@pruvisto.org>
2025-12-08 14:30:20 +0100

    Tuned

--- aoc8.hs
+++ aoc8.hs
@@ -1,3 +1,4 @@
+import Control.Arrow
 import Control.Monad
 import Data.List
 import Data.List.Split
@@ -13,18 +14,26 @@ parsePos :: String -> Pos
 parsePos s = (x, y, z)
   where [x, y, z] = map read $ splitOn "," s
 
+{-
+  Given a list `[x1, x2, ...]` compute the list `[(x_i, x_j) | i < j]`.
+-}
 pairs :: [a] -> [(a,a)]
 pairs [] = []
 pairs (x : xs) = [(x,y) | y <- xs] ++ pairs xs
 
 main1 = 
   do ps <- fmap (map parsePos . lines) getContents
-     let es = map fst $ sortOn snd [((p, q), sqDist p q) | (p, q) <- pairs ps]
+     let es = map fst . sortOn snd . map (id &&& uncurry sqDist) $ pairs ps
      let cls = runEquivM S.singleton S.union $ 
                  forM_ (take 1000 es) (uncurry equate) >> classes >>= mapM desc
      print . product . take 3 . sortBy (flip compare) . map S.size $ cls
 
-kruskal :: Ord a => Int -> [((a,a), Int)] -> [(a,a)]
+{-
+  Given a graph with `n` nodes and a list of weighted undirected edges, compute
+  returns the list of edges in a minimum-weight spanning forest of the graph.
+  If the graph is connected: aborts early once all edges have been found.
+-}
+kruskal :: (Ord a, Ord b) => Int -> [((a,a), b)] -> [(a,a)]
 kruskal nNodes es = runEquivM' $ go nNodes (map fst (sortOn snd es))
   where go n es | n <= 1 || null es = return []
         go n (e@(u,v) : es) =
@@ -33,7 +42,7 @@ kruskal nNodes es = runEquivM' $ go nNodes (map fst (sortOn snd es))
 
 main2 = 
   do ps <- fmap (map parsePos . lines) getContents
-     let es = [((p, q), sqDist p q) | (p, q) <- pairs ps]
+     let es = map (id &&& uncurry sqDist) (pairs ps)
      let ((x1, _, _), (x2, _, _)) = last (kruskal (length ps) es)
      print $ x1 * x2
 
